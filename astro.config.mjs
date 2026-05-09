56592c23b7886116226cdca6e4179f3fd422b84e
etalcott <edwardtalcott@hotmail.com>
2026-05-08 22:01:25 -0700

    Attempts to switch to static pages to work around worker size error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,10 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare({
-    imageService: 'cloudflare'
-  }),
+  output: 'static',
   build: {
     inlineStylesheets: 'always'
   },
